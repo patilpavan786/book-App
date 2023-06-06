7ce7b9a8b0d4dcc2bd9bea2f726f6d05f4a769e8
unknown <pavan.p.b.pavan@gmail.com>
2023-06-06 12:32:27 +0530

    css added to cart page

--- src/Component/Menu/Menu.js
+++ src/Component/Menu/Menu.js
@@ -6,8 +6,9 @@ function Menu() {
   return (
     <div className={style.headercontent} >
     <h2 className={style.headertitle}>find your book of choice.</h2><br />
+    <SearchForm />
     <p className={style.headertext }>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae sapiente quibusdam consequatur perspiciatis facere laboriosam non nesciunt at id repudiandae, modi iste? Eligendi, rerum!</p>
-    <SearchForm/>
+  
 </div>
   )
 }
